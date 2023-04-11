7259f7fb226278f91012f71c95d12bf59add8e67
Max Lechich <lechichm@gmail.com>
2023-04-11 19:59:52 -0400

    Defaults to non-verbose output.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-#![allow(unused)]
-use sha2::{Sha256, Sha512, Digest};
+use sha2::{Sha256, Digest};
 use std::{env, fs::File, io::Read};
 fn main() {
     let args:Vec<String> = env::args().collect();
     let mut checksum :String = String::new();
     let mut file_path: String = String::new();
+    let mut verbose: bool = false;
     //Parsing arguments
     if args.len() > 1 {
         for i in 0..args.len() {
@@ -13,19 +13,29 @@ fn main() {
                 "-c" => checksum = args[i+1].clone(),
                 //The file to be hashed
                 "-f" => file_path = args[i+1].clone(),
+                "-v" => verbose = true,
                 _ => (),
             }
         }
     }
     //Doing the thing that this thing does.
-    println!("Sha256 hash supplied:");
     let hash: String = retrieve_checksum(&checksum).unwrap();
-    //println!("{}", retrieve_checksum(&checksum).unwrap());
-    println!("{}", hash);
-    println!("Sha256 of input file:");
+    let result;
     //let result = git_sum_sha256_from_file(&file_path).unwrap();
-    println!("{}", git_sum_sha256_from_file(&file_path).unwrap());
-    //if
+    if verbose {
+        println!("Sha256 hash supplied:");
+        println!("{}", hash);
+        println!("Sha256 of the file you are checking:");
+        result = git_sum_sha256_from_file(&file_path).unwrap();
+        println!("{}", result);
+    } else {
+        result = git_sum_sha256_from_file(&file_path).unwrap();
+    }
+    if hash == result {
+        println!("They match");
+    } else {
+        println!("They do not match");
+    }
     //Below, testing part
     //test();
 }
@@ -35,11 +45,10 @@ fn git_sum_sha256_from_file( input:&String ) -> Result<String, String> {
         Ok(x) => x,
         Err(_) => panic!("Couldn't open file {input}"),
     };
-    //let mut file_string = String::new();
-    //file.read_to_string(&mut file_string);
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer);
+    let _x = file.read_to_end(&mut buffer);
     let mut summer = Sha256::new();
+    //update method takes forever. Finalize and read_to_end are effectively instant.
     summer.update(&buffer);
     Ok(format!("{:x}", summer.finalize()))
 }
@@ -51,27 +60,9 @@ fn retrieve_checksum(input:&String) -> Result<String, String> {
         Err(_) => panic!("Couldn't open file {input}"),
     };
     let output = match file.read_to_string(&mut file_string) {
-        Ok(x) => file_string.split(' ').next().unwrap().to_string(),
+        Ok(_) => file_string.split(' ').next().unwrap().to_string(),
         //Ok(x) => checksum.split_whitespace().collect()[0],
         Err(_) => panic!("Couldn't read checksum file to string."),
     };
     Ok(output)
-}
-
-//For testing purposes.
-fn sum_sha256_str(input:&String) -> Result<String, String> {
-    let mut summer = Sha256::new();
-    summer.update(&input);
-    let result = summer.finalize();
-    Ok(format!("{:x}", result))
-}
-
-//For testing purposes.
-fn test() {
-    let string_1 = String::from("Hello, World!\n");
-    let result = sum_sha256_str(&string_1);
-    println!("{}", result.unwrap());
-    let path = String::from("/home/meat/Projects/Rust/summer_of_our_disc_content/src/file");
-    let result = git_sum_sha256_from_file(&path);
-    println!("{}", result.unwrap());
 }
