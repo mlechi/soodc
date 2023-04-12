685d5653b91e658e6ed63347ff3269a7cfa3ac6a
Max Lechich <lechichm@gmail.com>
2023-04-11 20:23:35 -0400

    Added help message

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,24 @@ use sha2::{Sha256, Digest};
 use std::{env, fs::File, io::Read};
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let mut checksum :String = String::new();
+    let mut hash :String = String::new();
     let mut file_path: String = String::new();
     let mut verbose: bool = false;
     //Parsing arguments
     if args.len() > 1 {
         for i in 0..args.len() {
             match args[i].as_str() {
-                //The supplied checksum
-                "-c" => checksum = args[i+1].clone(),
+                //The supplied hash
+                "-h" => hash = args[i+1].clone(),
                 //The file to be hashed
                 "-f" => file_path = args[i+1].clone(),
                 "-v" => verbose = true,
                 _ => (),
             }
         }
-    }
+    } else { help_message(); return }
     //Doing the thing that this thing does.
-    let hash: String = retrieve_checksum(&checksum).unwrap();
+    let hash: String = retrieve_hash(&hash).unwrap();
     let result;
     //let result = git_sum_sha256_from_file(&file_path).unwrap();
     if verbose {
@@ -53,7 +53,7 @@ fn git_sum_sha256_from_file( input:&String ) -> Result<String, String> {
     Ok(format!("{:x}", summer.finalize()))
 }
 
-fn retrieve_checksum(input:&String) -> Result<String, String> {
+fn retrieve_hash(input:&String) -> Result<String, String> {
     let mut file_string = String::new();
     let mut file = match File::open(&input) {
         Ok(x) => x,
@@ -61,8 +61,19 @@ fn retrieve_checksum(input:&String) -> Result<String, String> {
     };
     let output = match file.read_to_string(&mut file_string) {
         Ok(_) => file_string.split(' ').next().unwrap().to_string(),
-        //Ok(x) => checksum.split_whitespace().collect()[0],
-        Err(_) => panic!("Couldn't read checksum file to string."),
+        //Ok(x) => hash.split_whitespace().collect()[0],
+        Err(_) => panic!("Couldn't read hash file to string."),
     };
     Ok(output)
+}
+
+fn help_message() {
+    println!("");
+    println!("This is the checkSummer Of Our Disk Content");
+    println!("");
+    println!("Requires a file to check, and a file containing the valid hash to compare the file to.");
+    println!("");
+    println!("-h: file containing the provided hash.");
+    println!("-f: file to be checked.");
+    println!("");
 }
